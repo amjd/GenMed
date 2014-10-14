81acee70656e58ea2b2b1144b76c6f3b3e8ffd0c
amjd <amjad.4@gmail.com>
2014-10-14 23:51:07 +0530

    Fix an edge case preventing Txtweb from recognizing the app

--- app.py
+++ app.py
@@ -89,6 +89,8 @@ class TrueMD(object):
 @app.route('/app/med_sugg')
 def med_sugg():
 	message = request.args['txtweb_message'].strip()
+	if not message:
+		return render_template("base.html", error_code=error_code, error_msg=error_msg, txtweb_key=TXTWEB_APP_KEY)
 	tmd = TrueMD()
 	suggestions, error_code, error_msg = tmd.med_suggestions(message)
 	if suggestions:
