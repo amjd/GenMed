2b9a66edc962b008f3f96101024d2fe969e8ad91
amjd <amjad.4@gmail.com>
2014-10-14 23:30:47 +0530

    Working implementation; modify app.py, add templates

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-import requests
+import os
 import urllib
 import json
-import os
+import requests
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
@@ -9,10 +9,10 @@ app = Flask(__name__)
 TXTWEB_APP_KEY = os.environ['TXTWEB_APP_KEY']
 
 class TrueMD(object):
-	ENDPOINT = "http://www.truemd.in/api/"
-	API_KEY = os.environ['API_KEY']
 
 	def __init__(self):
+		self.ENDPOINT = "http://www.truemd.in/api/"
+		self.API_KEY = os.environ['API_KEY']
 		self.params = {}
 
 	def __params(self, search_term, limit = None):
@@ -20,6 +20,7 @@ class TrueMD(object):
 		self.params['key'] = self.API_KEY
 		if limit:
 			self.params['limit'] = limit
+		print repr(self.params)
 
 	def med_suggestions(self, search_term, limit = 5):
 		"""
@@ -31,14 +32,18 @@ class TrueMD(object):
 		self.__params(search_term, limit)
 		suggestions_list = []
 		try:
-			request = requests.get(url, params=params)
+			request = requests.get(url, params=self.params)
 			result = request.json()
 			if result['status'] == 'ok' or result['status'] == 200:
-				for item in result['response']['suggestions']:
-					suggestions_list.append(item['suggestion'])
-			return suggestions_list
+				error_code = 0
+				error_msg = "Success"
+				temp_list = result['response']['suggestions']
+				suggestions_list = [item['suggestion'] for item in temp_list]
+
 		except:
-			return suggestions_list
+			error_code = 1
+			error_msg = "Sorry, we are experiencing some connection issues right now. Please try again later."
+		return suggestions_list, error_code, error_msg
 
 	def med_details(self, search_term):
 		"""
@@ -50,7 +55,7 @@ class TrueMD(object):
 		self.__params(search_term)
 		medicine_details = {}
 		try:
-			request = requests.get(url, params=params)
+			request = requests.get(url, params=self.params)
 			result = request.json()
 			if result['status'] == 'ok' or result['status'] == 200:
 				medicine_details = result['response']
@@ -68,44 +73,62 @@ class TrueMD(object):
 		self.__params(search_term, limit)
 		alternatives_list = []
 		try:
-			request = requests.get(url, params=params)
+			request = requests.get(url, params=self.params)
 			result = request.json()
 			if result['status'] == 'ok' or result['status'] == 200:
-				for item in result['response']['medicine_alternatives']:
-					alternatives_list.append(item)
-			return alternatives_list
+				temp_list = result['response']['medicine_alternatives']
+				alternatives_list = [{'brand': item['brand'], 'unit_price' : item['unit_price']} for item in temp_list]
+				error_code = 0
+				error_msg = "Success"
 		except:
-			return alternatives_list			
+			error_code = 1
+			error_msg = "Sorry, we are experiencing some connection issues right now. Please try again later."
 
-@app.route('/app')
-def index():
+		return alternatives_list, error_code, error_msg
+
+@app.route('/app/med_sugg')
+def med_sugg():
 	message = request.args['txtweb_message'].strip()
 	tmd = TrueMD()
-	suggestions = tmd.med_suggestions(message)
-	result = ""
+	suggestions, error_code, error_msg = tmd.med_suggestions(message)
 	if suggestions:
-		result = "Did you mean:<br>"
-		for suggestion in suggestion:
-			result = result + '<a href="/app/med_alt/%s">%s</a><br>' % (suggestion, suggestion)
+		return render_template("suggestions.html", suggestions=suggestions, error_code=error_code, \
+			error_msg=error_msg, txtweb_key=TXTWEB_APP_KEY)
+	else:
+		error_code = 2
+		error_msg = "We couldn't find a medicine by that name."
+		return render_template("suggestions.html", suggestions=suggestions, error_code=error_code, \
+			error_msg=error_msg, txtweb_key=TXTWEB_APP_KEY)
+
+#UNFINISHED
+@app.route('/app/med_detl')
+def med_detl():
+	message = request.args['txtweb_message'].strip()
+	tmd = TrueMD()
+	details = tmd.med_details(message)
+	result = ""
+	if details:
+		for suggestion in suggestions:
+			result = result + '<a href="/app/med_alt/%s">%s</a>\n' % (suggestion, suggestion)
 	else:
 		result = "We couldn't find a medicine by that name."
 
-	return render_template("base.html", result=result, textweb_key = TXTWEB_APP_KEY)
+	return render_template("suggestions.html", result=result.split('\n'), txtweb_key=TXTWEB_APP_KEY)
 
 @app.route('/app/med_alt/<med_name>')
 def med_alt(med_name):
-	med_name = med_name.replace('%20',' ')
+	med_name = urllib.unquote(med_name.encode('ascii')).decode('utf-8')
+	print med_name
 	tmd = TrueMD()
-	alternatives = tmd.med_alternatives(med_name)
-	result = ""
+	alternatives, error_code, error_msg = tmd.med_alternatives(med_name)
 	if alternatives:
-		result = "Equivalent generic medicines:<br>"
-		for alternative in alternatives:
-			result = result + "%s [Unit price: Rs. %s]<br>" % (alternative['brand'], alternative['unit_price'])
+			return render_template("alternatives.html", alternatives=alternatives, error_code=error_code, \
+			error_msg=error_msg, txtweb_key=TXTWEB_APP_KEY)
 	else:
-		result = "We couldn't find a generic alternative for that medicine."
-
-	return render_template("base.html", result=result, textweb_key = TXTWEB_APP_KEY)
+		error_code = 2
+		error_msg = "We couldn't find a generic alternative for that medicine."
+		return render_template("suggestions.html", suggestions=suggestions, error_code=error_code, \
+			error_msg=error_msg, txtweb_key=TXTWEB_APP_KEY)
 
 if __name__ == '__main__':
 	app.run(debug=True)
